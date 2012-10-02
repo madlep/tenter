958288b4d307114912322893b2ccd46bb916e2ca
madlep <madlep@madlep.com>
2012-10-02 23:46:35 +1000

    getting stuff to compile

---
+++ include/tenter.hrl
@@ -0,0 +1,4 @@
+-record(tenter, {
+  servers, 
+  options
+}).
--- src/tenter.erl
+++ src/tenter.erl
@@ -1,1 +1,16 @@
 -module(tenter).
+-author('madlep@madlep.com').
+
+-include("tenter.hrl").
+
+-export([
+  create/2,
+  discover/2
+]).
+
+create(Servers, Options) ->
+  #tenter{servers=Servers, options=Options}.
+
+discover(Tenter, Url) ->
+  tenter_discovery:discover(Tenter, Url).
+

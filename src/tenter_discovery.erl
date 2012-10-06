5e3fe28d291e732ee8796638ebf973be18538745
madlep <madlep@madlep.com>
2012-10-07 01:50:52 +1000

    make it compile

--- src/tenter_discovery.erl
+++ src/tenter_discovery.erl
@@ -21,6 +21,7 @@ fetch_profile_urls(Url) ->
 maybe_from_header(Url) ->
   extract_link_header(ibrowse:send_req(Url, [], head)).
 
+% "<https://tent.titanous.com/profile>; rel=\"https://tent.io/rels/profile\", <https://titanous.tent.is/tent/profile>; rel=\"https://tent.io/rels/profile\", <https://tent.jonathan.cloudmir.com/profile>; rel=\"https://tent.io/rels/profile\""
 extract_link_header({ok, "200", Headers, _Body}) ->
   extract_tent_rel_link(proplists:get_value("Link", Headers));
 extract_link_header(_RequestFailed) ->
@@ -28,13 +29,11 @@ extract_link_header(_RequestFailed) ->
 
 extract_tent_rel_link(undefined) ->
   not_in_headers;
-extract_tent_rel_link(LinkHeader) ->
-
-
-
+extract_tent_rel_link(_LinkHeader) ->
+  foo.
 
 maybe_from_html_head(not_in_headers) ->
   foo;
 maybe_from_html_head(FoundUrlsInHeader) ->
-  FoundUrlsInHeader;
+  FoundUrlsInHeader.
 

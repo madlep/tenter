7ab461aa6dd3b257c6f04f2ddd12734648a190ab
Julian Doherty (madlep) <madlep@madlep.com>
2012-11-18 11:34:56 +1100

    simplify

--- src/tenter_app.erl
+++ src/tenter_app.erl
@@ -6,7 +6,11 @@
 -export([start/0, start/2, stop/1]).
 
 % from http://stackoverflow.com/questions/10502783/erlang-how-to-load-applications-with-their-dependencies#10524644
-start() -> a_start(tenter, permanent).
+start() -> 
+  ensure_started(crypto),
+  ensure_started(public_key),
+  ensure_started(ssl),
+  ensure_started(ibrowse).
 
 start(_Args, _Type) ->
   start().
@@ -14,13 +18,10 @@ start(_Args, _Type) ->
 stop(_State) ->
   ok.
 
-a_start(App, Type) ->
-    start_ok(App, Type, application:start(App, Type)).
-
-start_ok(_App, _Type, ok) -> ok;
-start_ok(_App, _Type, {error, {already_started, _App}}) -> ok;
-start_ok(App, Type, {error, {not_started, Dep}}) ->
-    ok = a_start(Dep, Type),
-    a_start(App, Type);
-start_ok(App, _Type, {error, Reason}) ->
-    erlang:error({app_start_failed, App, Reason}).
+ensure_started(App) ->
+  case application:start(App) of
+    ok ->
+      ok;
+    {error, {already_started, App}} ->
+      ok
+  end.

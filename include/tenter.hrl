c0992bf56de7c611a10593a8066e356a26e30ba7
Julian Doherty (madlep) <madlep@madlep.com>
2012-12-13 00:27:09 +1100

    added basic/core profile records

--- include/tenter.hrl
+++ include/tenter.hrl
@@ -2,3 +2,34 @@
   servers, 
   options
 }).
+
+-record(post_profile, {
+  info_basic,
+  info_core
+}).
+
+-record(info_basic, {
+  avatar_url,
+  bio,
+  birthdate,
+  gender,
+  location,
+  name,
+  permissions,
+  version
+}).
+
+-record(info_core, {
+  entity,
+  licenses,
+  permissions,
+  servers,
+  tent_version,
+  version
+}).
+
+-record(birthdate, {
+  year,
+  month,
+  day
+}).
